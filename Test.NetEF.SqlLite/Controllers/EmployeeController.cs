c47d8e270b6eb5afdccb6ec2c2baeda2b23964e9
johannpa <johann_paque@hotmail.fr>
2023-10-01 16:19:02 +0200

    Add migration, update db, create db in sqlite and add a controller for employee

---
+++ Test.NetEF.SqlLite/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Test.NetEF.SqlLite.Data;
+
+namespace Test.NetEF.SqlLite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public EmployeeController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+    }
+}
