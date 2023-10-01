82c8f8f18a68e3acaef9f7179268d5a037366300
johannpa <johann_paque@hotmail.fr>
2023-10-01 17:43:48 +0200

    Create DeleteEmployee and UpdateEmployee

--- Test.NetEF.SqlLite/Controllers/EmployeeController.cs
+++ Test.NetEF.SqlLite/Controllers/EmployeeController.cs
@@ -34,5 +34,50 @@ namespace Test.NetEF.SqlLite.Controllers
             }
             return Ok(employee);
         }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Employee>>> AddEmployee(Employee newEmployee)
+        {
+            if (newEmployee != null)
+            {
+                _appDbContext.Employees.Add(newEmployee);
+                await _appDbContext.SaveChangesAsync();
+                return await _appDbContext.Employees.ToListAsync();
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<List<Employee>>> DeleteEmployee(int id)
+        {
+            var employee = await _appDbContext.Employees.FirstOrDefaultAsync(e => e.Id == id);
+            if (employee != null)
+            {
+                _appDbContext.Employees.Remove(employee);
+                await _appDbContext.SaveChangesAsync();
+                return await _appDbContext.Employees.ToListAsync();
+            }
+            return BadRequest();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Employee>> UpdateEmployee(Employee updatedEmployee)
+        {
+            if(updatedEmployee != null)
+            {
+                var employee = await _appDbContext.Employees.FirstOrDefaultAsync(e => e.Id == updatedEmployee.Id);
+                if (employee != null)
+                {
+                    employee.Name = updatedEmployee.Name;
+                    employee.Age = updatedEmployee.Age;
+                    await _appDbContext.SaveChangesAsync();
+
+                    var employees = await _appDbContext.Employees.ToListAsync();
+                    return Ok(employees);
+                }
+                return NotFound();
+            }
+            return BadRequest();
+        }
     }
 }
